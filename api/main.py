085becc01ac7e0a1eeee0eb2b7d55dca04dc5331
jfan4926 <jfan0126@gmail.com>
2026-05-28 09:02:52 -0500

    update main.py comment

--- api/main.py
+++ api/main.py
@@ -13,10 +13,10 @@ from dotenv import load_dotenv
 from fastapi.security import APIKeyHeader
 from fastapi import Security
 load_dotenv()
-#render debug
-import sys
-print(f"Starting up, Python {sys.version}", flush=True)
-DB_PATH = "data/processed/nyc311.duckdb"
+# #render debug
+# import sys
+# print(f"Starting up, Python {sys.version}", flush=True)
+# DB_PATH = "data/processed/nyc311.duckdb"
 
 limiter = Limiter(key_func=get_remote_address)
 
